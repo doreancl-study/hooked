54ed466a1583f8d4f21e7209e664bcce47cae3e7
DoreanCl <doreancl@gmail.com>
2021-08-16 23:06:55 -0400

    fixing all files

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
-const DEFAULT_PLACEHOLDER_IMAGE = "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+const DEFAULT_PLACEHOLDER_IMAGE =
+  'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg';
 
 const Movie = ({movie}) => {
-  const poster = movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
+  const poster =
+    movie.Poster === 'N/A' ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
 
   return (
     <div className="movie">
@@ -17,7 +19,7 @@ const Movie = ({movie}) => {
       </div>
       <p>({movie.Year})</p>
     </div>
-  )
-}
+  );
+};
 
-export default Movie
+export default Movie;
